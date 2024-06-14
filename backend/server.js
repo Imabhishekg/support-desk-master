c0afaee138b2d13673378a9354f52a4c32a60256
Imabhishekg <138770761+Imabhishekg@users.noreply.github.com>
2024-06-14 12:08:42 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const colors = require('colors')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 5000
+const {errorHandler} = require('./middleware/errorMiddleware') 
+const connectDB = require('./config/db')
+const app = express()
+
+//connect to database
+connectDB()
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Welcome to support desk API'})
+})
+
+app.use('/api/users', require('./routes/userRoutes'))
+app.use('/api/tickets', require('./routes/ticketRoutes'))
+app.use(errorHandler)
+app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`))  
